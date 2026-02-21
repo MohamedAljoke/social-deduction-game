8b05166b0b6661a3ccbd3b05d77434c785dfae96
mohamed@idip.com.br <mohamed@idip.com.br>
2026-02-21 00:27:24 -0300

    feat: phases

--- src/match.ts
+++ src/match.ts
@@ -1,5 +1,6 @@
 import crypto from "node:crypto";
 import { PlayerNotFound } from "./errors";
+import { Phase, PHASE_ORDER, PhaseType } from "./phase";
 
 type Player = {
   id: string;
@@ -7,7 +8,16 @@ type Player = {
 };
 
 export class Match {
-  public players: Player[] = [];
+  private players: Player[] = [];
+  private phase: Phase = new Phase();
+
+  public getCurrentPhase(): PhaseType {
+    return this.phase.getCurrentPhase();
+  }
+
+  public nextPhase(): PhaseType {
+    return this.phase.nextPhase();
+  }
 
   public addPlayer(name: string): void {
     const id = crypto.randomUUID();
--- src/test/match.spec.ts
+++ src/test/match.spec.ts
@@ -1,6 +1,7 @@
 import { describe, expect, test } from "vitest";
 import { Match } from "../match";
 import { PlayerNotFound } from "../errors";
+import { PHASE_ORDER } from "../phase";
 
 describe("match", () => {
   test("should get all players", () => {
@@ -27,5 +28,22 @@ describe("match", () => {
     const match = new Match();
 
     expect(() => match.getPlayerByID("random_id")).toThrow(PlayerNotFound);
+  });
+
+  test("should start a match in discussion phase", () => {
+    const match = new Match();
+
+    expect(match.getCurrentPhase()).toBe("discussion");
+  });
+
+  test("should go to next phase correctly", () => {
+    const match = new Match();
+
+    PHASE_ORDER.forEach((_, index) => {
+      match.nextPhase();
+      expect(match.getCurrentPhase()).toBe(
+        PHASE_ORDER[(index + 1) % PHASE_ORDER.length],
+      );
+    });
   });
 });
